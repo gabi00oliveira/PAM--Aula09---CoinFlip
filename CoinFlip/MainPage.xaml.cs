5d5483504a50a7887924c061c3b104b6ba5ce142
Gabrielle <gabriellevasconcelosoliveira@gmail.com>
2025-04-11 16:39:47 -0300

    DS - Aula 09 - CoinFlip funcionando

--- CoinFlip/MainPage.xaml.cs
+++ CoinFlip/MainPage.xaml.cs
@@ -1,76 +1,54 @@
-﻿namespace CoinFlip
+﻿    using Microsoft.Maui.Controls.Platform;
+    using Microsoft.Maui.Controls.PlatformConfiguration.iOSSpecific;
+
+namespace CoinFlip
 {
     public partial class MainPage : ContentPage
     {
         int count = 0;
-
-        public bool Cara { get; private set; }
-        public bool Coroa { get; private set; }
 
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+
+        private async void FlipButton_Clicked(object sender, EventArgs e)
         {
-            
+
+            //Algoritmo
+            //Verificar qual opcao esta selecionada
+            //Fazer o sorteio da moeda  (Random.Next(2))
+            //Exibir o lado que foi sorteado (alterar a propriedade, nome do objeto.classe) MoedaImage.Source="coroa.png"
+            //Verificar se a opção é igual ao lado escolhido
+            //Exibir o resultado do jogo em um alerta (DisplayAlert)
+            int selectedIndex = SelecaoPicker.SelectedIndex;
+
+            Random rnd = new Random();
+
+            int sorteio = rnd.Next(2);
+
+            if (sorteio == 0)
+            {
+                MoedaImg.Source = "cara.png";
+            }
+            else
+            {
+                MoedaImg.Source = "coroa.png";
+            }
+
+            if (selectedIndex == sorteio )
+            {
+                await DisplayAlert("Parabéns!", "Você ganhou!", "OK");
+            }
+            else
+            {
+                await DisplayAlert("Que pena!", "Você perdeu. Tente novamente.", "OK");
+            }
+
+
+
+
         }
-
-
-            //Algoritmo
-            //Verificar qual opção está selecionada. (if alguma coisa)
-            //Fazer o sorteio da moeda (Random)
-            //Exibir o lado que foi sorteado (Alterar a propriedade)
-            //Verificar se a opção é igual ao lado escolhido (if)
-            //Exibir o resultado do jogo em um alerta (DisplayAlert)
-
-            private void Girar_Clicked(object sender, EventArgs e)
-            {
-            //Algoritmo
-            //Verificar qual opção está selecionada. (if alguma coisa)
-            //Fazer o sorteio da moeda (Random)
-            //Exibir o lado que foi sorteado (Alterar a propriedade)
-            //Verificar se a opção é igual ao lado escolhido (if)
-            //Exibir o resultado do jogo em um alerta (DisplayAlert)
-
-
-            // Verifica qual opção está selecionada
-            string escolhaUsuario = "";
-            if (Cara)
-            {
-                escolhaUsuario = "Cara";
-            }
-            else if (Coroa)
-            {
-                escolhaUsuario = "Coroa";
-            }
-            else
-            {
-                DisplayAlert("Atenção", "Selecione Cara ou Coroa antes de girar.", "OK");
-                return;
-            }
-
-            // Sorteia o lado da moeda
-            Random random = new Random();
-            string resultado = random.Next(2) == 0 ? "" : "Coroa";
-
-            // Atualiza a imagem (opcional - você precisa ter as imagens no seu projeto)
-            coroa.Source = resultado == "Cara" ? "cara.webp" : "coroa.png";
-
-            // Verifica se o usuário acertou
-            if (escolhaUsuario == resultado)
-            {
-                DisplayAlert("Resultado", $"Deu {resultado}! Você acertou!", "OK");
-            }
-            else
-            {
-                DisplayAlert("Resultado", $"Deu {resultado}! Você errou!", "OK");
-            }
-            
-            }
-
     }
 }
-
-
